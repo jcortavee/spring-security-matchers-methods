fcd49d007fe91dfee62d965c41e327fd0b8b9e74
Juan Carlos Ramirez <jccortavee@gmail.com>
2022-06-19 16:56:14 -0600

    Matchers methods in Spring Security

---
+++ src/test/java/com/josecarlos/securitymatchermethods/SecurityMatcherMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josecarlos.securitymatchermethods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMatcherMethodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
